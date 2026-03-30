5967d7a8f7a313bb897f5ad0057c88cc8dbba327
Vasco273 <vasco3638@gmail.com>
2026-03-30 09:59:55 +0630

    Fix bot responses and update main check

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import os
 import telebot
 
-# Railway Variables ထဲက TOKEN ကို ဖတ်ခိုင်းတာပါ
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "🤩 MLBB Diamonds Bot မှ ကြိုဆိုပါတယ်ဗျာ!\n\nဈေးနှုန်းမေးမြန်းရန် /price ကို နှိပ်ပါ။")
+    bot.reply_to(message, "MLBB Diamonds Bot မှ ကြိုဆိုပါတယ်ဗျာ!\n\nစျေးနှုန်းမေးမြန်းရန် /price ကို နှိပ်ပါ။")
 
 @bot.message_handler(commands=['price'])
-def send_price(‌price):
-    prices = (
-        "💎 MLBB Diamonds Price List 💎\n\n"
+def send_price(message):
+    prices = (5000)
+        "MLBB Diamonds Price List\n\n"
         "86 Diamonds - 2,500 MMK\n"
         "172 Diamonds - 4,800 MMK\n"
         "257 Diamonds - 7,200 MMK\n\n"
@@ -22,10 +21,8 @@ def send_price(‌price):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(, "ခ‌ဏတော့စောင့်ပေးပါဗျ")
+    bot.reply_to(message, "စာပြန်ဖို့အတွက် command များကို အသုံးပြုပါဗျ။")
 
-# Bot ကို စတင် Run ခိုင်းတာပါ
-if __name__ == "__အ‌ေကာင့်__ID(sever)__":
-    print("Bot is starting...")
+if __name__ == "__main__":
     bot.infinity_polling()
     
