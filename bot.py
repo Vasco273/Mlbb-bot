b8fa32a2e6d17b0c316706a9618f49f3a4316f73
Vasco273 <vasco3638@gmail.com>
2026-03-30 09:20:05 +0630

    Update environment variable name for bot token

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # Token ကို Render ရဲ့ Environment Variable ကနေ ဖတ်မှာဖြစ်ပါတယ်
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv('TOKEN')
 
 # Diamond ဈေးနှုန်းဇယား
 PRICES = {
